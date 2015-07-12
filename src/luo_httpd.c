04efe7701d5646bb7e9545050893739fbacb7dda
wuleying <5590548@qq.com>
2015-07-12 12:50:37 +0800

    update

--- src/luo_httpd.c
+++ src/luo_httpd.c
@@ -1,8 +1,90 @@
 #include "luo_httpd.h"
 
-int main()
+int luo_startup(u_short *port)
 {
-	printf("%s\n", "test");
+	int httpd = 0;
+	luo_sockaddr_in name;
+
+	httpd = socket(PF_INET, SOCK_STREAM, 0);
+	if (httpd == LUO_ERROR)
+	{
+		luo_error("Socket error");
+	}
+
+	memset(&name, 0, sizeof(luo_sockaddr_in));
+
+	name.sin_family = AF_INET;
+	name.sin_port = htons(*port);
+	name.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(httpd, (luo_sockaddr *) &name, sizeof(name)) < 0)
+	{
+		luo_error("Bind error");
+	}
+
+	if (*port == 0)
+	{
+		socklen_t name_len = sizeof(name);
+
+		if (getsockname(httpd, (luo_sockaddr *) &name, &name_len) == LUO_ERROR)
+		{
+			luo_error("Get socket name error");
+		}
+
+		*port = ntohs(name.sin_port);
+	}
+
+	if (listen(httpd, 5) < 0)
+	{
+		luo_error("Listen error");
+	}
+
+	return (httpd);
+}
+
+void *
+accept_request(void *tclient)
+{
+	return NULL;
+}
+
+// 输出错误信息
+void luo_error(const char *error)
+{
+	perror(error);
+	exit(1);
+}
+
+// 主函数
+int main(void)
+{
+	int server_sock = SOCK_DEFAULT;
+	u_short port = 0;
+	int client_sock = SOCK_DEFAULT;
+	socklen_t client_name_len = sizeof(luo_sockaddr_in);
+	pthread_t new_thread;
+
+	server_sock = luo_startup(&port);
+	printf("luo_httpd running on port %d\n", port);
+
+	while (1)
+	{
+		client_sock = accept(server_sock, (luo_sockaddr *) &client_sock,
+				&client_name_len);
+
+		if (client_sock == LUO_ERROR)
+		{
+			luo_error("Accept error");
+		}
+
+		if (pthread_create(&new_thread, NULL, accept_request,
+				(void *) &client_sock) != 0)
+		{
+			luo_error("Create error");
+		}
+	}
+
+	close(server_sock);
 
 	return 0;
 }
--- src/luo_httpd.h
+++ src/luo_httpd.h
@@ -1,8 +1,46 @@
 #ifndef _LUO_HTTPD_H_INCLUDED_
 #define _LUO_HTTPD_H_INCLUDED_
 
+/* 头文件 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+// 互联网地址族头文件(Internet address family)
+#include <netinet/in.h>
+// 互联网操作的定义(Definitions for internet operations)
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <strings.h>
+#include <string.h>
+#include <pthread.h>
+
+/* 常量/宏声明 */
+// 服务描述
+#define SERVER_DESC 	"Server: luo_httpd/0.0.1\r\n"
+
+// sock默认值
+#define SOCK_DEFAULT 	-1
+
+// 状态码
+#define LUO_OK 		 	 0
+#define LUO_ERROR 		-1
+
+// 判断字符x是否为空白符
+#define luo_isspace 	isspace((int) x)
+
+/* 结构体声明 */
+typedef struct sockaddr_in luo_sockaddr_in;
+typedef struct sockaddr luo_sockaddr;
+
+/* 函数声明 */
+int luo_startup(u_short *port);
+void *accept_request(void *tclient);
+// 输出错误信息
+void luo_error(const char *error);
+
 
 #endif /* _LUO_HTTPD_H_INCLUDED_ */
